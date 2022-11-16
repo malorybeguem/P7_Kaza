944d4f8bcb924f014a3aeec01915ac7e5de1c620
malory.beguem <euphorya@outlook.jp>
2022-11-16 22:32:02 +0100

    some adds, header js and prettier

---
+++ src/components/header.js
@@ -0,0 +1,25 @@
+// IMPORTATIONS //
+import React from "react" // REACT IMPORT
+import {NavLink} from "react-router-dom"
+import Logo from "../assets/logo/Logo.svg" // LOGO IMPORT 
+
+// HEADER HTML CONSTITUTION //
+const Header = () => {
+    return ( // HTML CODE FOR THE HEADER //
+    <header> 
+            <img className="logo" src={Logo} alt="Logo Kasa" />
+            <nav>
+                <ul>
+                    <NavLink to="/">
+                        <li>Accueil</li>
+                    </NavLink>
+                    <NavLink to="/a-propos">
+                        <li>A Propos</li>
+                    </NavLink>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
